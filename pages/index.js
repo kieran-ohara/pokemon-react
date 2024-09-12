f68460c85882034ea1a5c15cb04f9beafbebd9a0
Kieran O'Hara <kieranohara@Kierans-MacBook-Pro.local>
2024-09-12 23:50:55 +0100

    Lint index.js.

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,7 @@ export default function Home() {
         </p>
 
         <p className={styles.description}>
-          <Link href="/posts/first-post">
-            Goto first page
-          </Link>
+          <Link href="/posts/first-post">Goto first page</Link>
         </p>
 
         <div className={styles.grid}>
@@ -99,15 +97,8 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
@@ -116,17 +107,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
         * {
