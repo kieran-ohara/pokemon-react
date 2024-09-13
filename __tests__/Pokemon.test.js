f7c9bce6d2c71704d47a64aa107567082262a80d
Kieran O'Hara <hello@kieranohara.me>
2024-09-13 09:54:57 +0100

    Add react-testing-library on the pokemon page.

---
+++ __tests__/Pokemon.test.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom'
+import Home from '../app/page'
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', { level: 1 })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
