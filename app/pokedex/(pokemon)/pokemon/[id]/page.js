d6f4c687e1801a3feec3744be0533701a3f3eed8
Kieran O'Hara <kieranohara@Kierans-MacBook-Pro.local>
2024-09-13 09:31:17 +0100

    Add some metadata.

--- app/pokedex/(pokemon)/pokemon/[id]/page.js
+++ app/pokedex/(pokemon)/pokemon/[id]/page.js
@@ -1,5 +1,13 @@
 import Pokemon from '../../../../../components/Pokemon';
 
+export async function generateMetadata({ params }) {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`).then(res => res.json());
+  return {
+    title: `${pokemon.name} - Pokemon Details`,
+    description: `View details for ${pokemon.name}, a ${pokemon.types.map(t => t.type.name).join('/')} type Pokemon.`
+  };
+}
+
 export default function PokemonPage({ params }) {
   return (
     <div>
